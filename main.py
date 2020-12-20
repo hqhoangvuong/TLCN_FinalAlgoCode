c10a41297be4698b8e85ff575db376e5659b671e
Vuong Huynh <17110256@student.hcmute.edu.vn>
2020-12-20 06:52:07 +0000

    done some minor fix

--- main.py
+++ main.py
@@ -7,39 +7,42 @@ Created on Sun Nov 29 09:53:48 2020
 
 import cv2
 from video_reader import VideoReader
-from frame_enhancer import LowLightEnhance
 from face_detector import FaceDetector
 from face_tagger import CentroidTracker
 from video_writer import VideoWriter
+import time
 
 video_reader = VideoReader()
-low_light_enhancer = LowLightEnhance('snapshots/Epoch99.pth', 0)
 face_detector = FaceDetector(gpu_id = 0)
-face_tagger = CentroidTracker(maxDisappeared = 25)
+face_tagger = CentroidTracker(maxDisappeared = 20)
 
-video_reader.setVideoPath(r'videos/video2.mp4')
+video_reader.setVideoPath(r'videos/video1.mp4')
 video_reader.setFrameSavePath(r'savedframes')
 
 def main():
     ret = True
     frame_dim = video_reader.getVideoDimension()
     video_writer = VideoWriter('abcd', frame_dim)
+    
+    start_time = time.time()
+    x = 1
+    counter = 0
+
     while ret:
         ret, frame, frame_no = video_reader.getFrame()
         if ret == False:
             break
-        
+        face_detector.detect(frame)
         rects = []
         frame, rects = face_detector.draw_boundary_box(frame)
         objects, maxAppereds = face_tagger.update(rects)
             
         for (objectID, centroid) in objects.items():
-            text = "ID {0}".format(objectID)
-            text_color = (255, 0, 0)
+            text_color = (0, 0, 255)
             
             if maxAppereds.get(objectID) >= 50:
                 text_color = (0, 255, 0)
-                
+            text = "ID {0}, Nguoi {1}".format(objectID, 'quen' if maxAppereds.get(objectID) >= 50 else 'la')
             cv2.putText(frame, 
                         text, 
                         (centroid[0] - 10, centroid[1] - 10),
@@ -48,15 +51,15 @@ def main():
                         text_color,
                         2)
             
-            cv2.circle(frame, 
-                       (centroid[0], centroid[1]), 
-                       4, 
-                       (0, 255, 0), 
-                       -1)
-            
         video_writer.writeFrame(frame)
         
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        counter += 1
+        if (time.time() - start_time) > x :
+            print("FPS: ", counter / (time.time() - start_time))
+            counter = 0
+            start_time = time.time()
+        
+        if 0xFF == ord('q'):
             break
 
 
